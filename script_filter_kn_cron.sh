3a8cd07af1e330c39ab7664317805b739b220f1a
igorandreoni <igor.andreoni@gmail.com>
2020-09-03 14:09:59 -0700

    Added script to trigger the crontab

---
+++ script_filter_kn_cron.sh
@@ -0,0 +1,23 @@
+#!/bin/tcsh
+
+setenv LD_LIBRARY_PATH /data/ia/anaconda3/lib
+setenv LD_RUN_PATH /data/ia/anaconda3/bin
+
+# Set path for ZTF forced photometry
+setenv PYTHONPATH /data/ia/
+setenv PYTHONPATH /data/ia/ForcePhotZTF:${PYTHONPATH}
+
+cd /data/ia/ztfrest/
+
+echo "Time:"
+echo `date`
+
+# Run the pipeline
+/data/ia/anaconda3/bin/python /data/ia/ztfrest/filter_kn.py --doWriteDb --doForcePhot --doCLU
+
+# Send a slack message about the completion of the data processing
+/data/ia/anaconda3/bin/python /data/ia/ztfrest/slack_bot_processing.py
+
+echo "Finished cron job"
+echo "Time:"
+echo `date`
