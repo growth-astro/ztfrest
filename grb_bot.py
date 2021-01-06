61f51d316fb91bc965f4650b462a238543d0cb81
ztfrest <michael.coughlin@gmail.com>
2021-01-06 08:03:43 -0800

    add grb plot

--- grb_bot.py
+++ grb_bot.py
@@ -25,6 +25,7 @@ from datetime import datetime
 
 import healpy as hp
 
+from ligo.skymap import plot, postprocess
 import requests
 from bs4 import BeautifulSoup
 
@@ -38,6 +39,112 @@ slack_token = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".slack_a
 with open(slack_token, "r") as f:
     access_token = f.read()
 web_client = WebClient(token=access_token)
+
+def GRB_unnoculted(path_GBM, path_out):
+    """ Create a new skymap with the earth-oculted portion of the map masked
+    
+    new map in path.replace('.fit','_unocculted.fit')
+    
+    haversine and find_greedy_credible_levels from GBM_tools 
+    https://fermi.gsfc.nasa.gov/ssc/data/analysis/rmfit/gbm_data_tools/gdt-docs/index.html
+    """
+    
+    def haversine(lon1, lat1, lon2, lat2, deg=True):
+        """Calculates the angular separation between two points using the
+        haversine equation. If degrees are passed, degrees are returned. else
+        the input/output is assumed to be radians.
+        lon -> azimuth
+        lat -> zenith
+
+        Args:
+            lon1 (float): lon/az of first point
+            lat1 (float): lat/zen of first point
+            lon2 (float): lon/az of second point
+            lat2 (float): lat/zen of second point
+            deg (bool, optional): True if input/output in degrees.
+
+        Returns:
+            float: Angular separation between points
+        """
+        if deg:
+            lon1, lat1, lon2, lat2 = map(np.deg2rad, [lon1, lat1, lon2, lat2])
+        d_lat = 0.5 * (lat2 - lat1)
+        d_lon = 0.5 * (lon2 - lon1)
+
+        a = np.sin(d_lat) ** 2 + (np.sin(d_lon) ** 2 * np.cos(lat1) * np.cos(lat2))
+        alpha = 2. * np.arctan2(np.sqrt(a), np.sqrt(1.0 - a))
+
+        if deg:
+            alpha = np.rad2deg(alpha)
+
+        return alpha
+
+    def find_greedy_credible_levels(p):
+        """Calculate the credible values of a probability array using a greedy
+        algorithm.
+
+        Args:
+            p (np.array): The probability array
+
+        Returns:    
+             np.array: The credible values
+        """
+        p = np.asarray(p)
+        pflat = p.ravel()
+        i = np.argsort(pflat)[::-1]
+        cs = np.cumsum(pflat[i])
+        cls = np.empty_like(pflat)
+        cls[i] = cs
+        return cls.reshape(p.shape)
+
+    # getting header information
+    hdu = fits.open(path_GBM)
+    geo_ra = hdu['HEALPIX'].header['GEO_RA']
+    geo_dec = hdu['HEALPIX'].header['GEO_DEC']
+    geo_location = [geo_ra,geo_dec]
+
+    try:
+        geo_radius = hdu['HEALPIX'].header['GEO_RAD']
+    except:
+        geo_radius = 67.5
+
+    prob, sig= hp.read_map(path_GBM, field=(0, 1), memmap=False,
+                                    verbose=False)
+    # prob_mask = prob > 0.0
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+
+    theta, phi = hp.pix2ang(nside, np.arange(npix))
+    ra = np.rad2deg(phi)
+    dec = np.rad2deg(np.pi / 2.0 - theta)
+    ang = haversine(*geo_location, ra, dec)
+
+    # masking earth
+    geo_mask = (ang <= geo_radius)
+    new_prob = np.copy(prob)
+    new_prob[geo_mask] = 0.0
+    # renormalize
+    new_prob /= np.sum(new_prob)
+    # have to redo the significance
+    new_sig = 1.0 - find_greedy_credible_levels(new_prob)
+
+    unocculted_path = path_GBM.replace('.fit','_unocculted.fit')
+    # get arrays in proper order, and write the healpix data to disk
+    prob_arr = hp.reorder(new_prob, r2n=True)
+    sig_arr = hp.reorder(new_sig, r2n=True)
+    columns = ['PROBABILITY', 'SIGNIFICANCE']
+    hp.write_map(unocculted_path, (prob_arr, sig_arr), nest=True, coord='C',
+                 overwrite=True, \
+                 column_names=columns,
+                 extra_header=hdu[1].header.cards)
+    hdulist = fits.open(unocculted_path)
+    hdulist[0].header.extend(hdu[0].header.cards)
+    hdulist[1].name = 'HEALPIX'
+    hdulist[1].header['TTYPE1'] = (
+    'PROBABILITY', 'Differential probability per pixel')
+    hdulist[1].header['TTYPE2'] = (
+    'SIGNIFICANCE', 'Integrated probability')
+    hdulist.writeto(path_out, overwrite=True, checksum=True)
 
 def in_out(map_path,ra_obj,dec_obj, top_fraction = 0.95 ):
     
@@ -71,6 +178,49 @@ def in_out(map_path,ra_obj,dec_obj, top_fraction = 0.95 ):
         inside = True 
     return inside
 
+def upload_fig(fig, user, filename, channel_id):
+    imgdata = io.BytesIO()
+    fig.savefig(imgdata, format='png', dpi=600, transparent=True)
+    imgdata.seek(0)
+    #wc = WebClient(token=bot_access_token)
+    web_client.files_upload(
+        file=imgdata.getvalue(),
+        filename=filename,
+        channels=channel_id,
+        text="<@{0}>, here's the file {1} I've uploaded for you!".format(user, filename)
+    )
+    #fig.close()
+
+def pretty_plot(map_path,ra,dec,cand_name='name'):
+
+    # loading relevant info
+    prob = hp.read_map(map_path)
+    ipix = np.argmax(prob)
+    npix = len(prob)
+    nside = hp.npix2nside(npix)
+    theta, phi = hp.pix2ang(nside, ipix)
+
+    # getting the center
+    ra_max = np.rad2deg(phi)
+    dec_max = np.rad2deg(0.5 * np.pi - theta)-10
+    center = str(int(SkyCoord(ra_max,dec_max,unit='deg').ra.hms.h))+'h '+ str(int(SkyCoord(ra_max,dec_max,unit='deg').dec.dms.d))+'d'
+    sky_area_per_pix = 4 * 180**2 / np.pi /npix
+    area_95 = np.sum(np.cumsum(-np.sort(-prob))<=0.95) * sky_area_per_pix
+    area_50 = np.sum(np.cumsum(-np.sort(-prob))<=0.5) * sky_area_per_pix
+    print('AREA 50% ',area_50)
+    print('AREA 95% ',area_95)
+
+    #plotting
+    fig = plt.figure()
+    ax = plt.axes(projection='astro globe', center=center)
+    ax.imshow_hpx(prob, cmap='cylon')
+    cl = postprocess.find_greedy_credible_levels(prob) * 100
+    ax.contour_hpx(cl, levels=[50, 95], colors=['black', 'black'], linewidths=[0.5, 0.5])
+    ax.grid()
+    ax.scatter(ra,dec,marker='*',color='blue',transform=ax.get_transform('world'),zorder=10)
+    ax.set_title(fits.open(map_path)[0].header['OBJECT']+' / '+str(fits.open(map_path)[0].header['TRIGTIME'])+' \n'+cand_name)
+
+    return fig
 
 def run_on_event(channel_id, bypass=False):
 
@@ -202,7 +352,11 @@ def run_on_event(channel_id, bypass=False):
                     mv_command = "mv %s %s" % (skymap.split("/")[-1], filename)
                     os.system(mv_command)
 
-                inout = in_out(filename, ra, dec, top_fraction = 0.95 )
+                unocculted_path = filename.replace('.fit','_unocculted.fit')
+                GRB_unnoculted(filename, unocculted_path)
+                inout = in_out(unocculted_path, ra, dec, top_fraction = 0.95 )
+                fig = pretty_plot(unocculted_path, ra, dec, cand_name=name)
+                upload_fig(fig, user, "skymap.png", channel_id)
 
                 message = []
                 message.append("Fermi Trigger number: %d" % (trignum))
